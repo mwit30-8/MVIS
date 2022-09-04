946fa1e9396843e79cd59a86a52a27fbda7fa0f9
wanorn <66821179+wanorn@users.noreply.github.com>
2022-09-04 15:20:27 +0700

    Update webpack.config.js
    Signed-off-by: wanorn <66821179+wanorn@users.noreply.github.com>

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,6 +5,9 @@ module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
   // Customize the config before returning it.
   config.plugins.push(new webpack.DefinePlugin({
+    'process.env.BACKEND_URL': JSON.stringify(process.env.BACKEND_URL),
+    'process.env.AUTH0_CLIENT_ID': JSON.stringify(process.env.AUTH0_CLIENT_ID),
+    'process.env.AUTH0_URL': JSON.stringify(process.env.AUTH0_URL),
     'process.env.SERVED_PATH': JSON.stringify(env.locations.servedPath)
   }));
   return config;
