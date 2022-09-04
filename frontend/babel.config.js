dc93028a289d9bad7a6e2079964bb8b2a4934711
wanorn <66821179+wanorn@users.noreply.github.com>
2022-09-04 15:14:09 +0700

    Update babel.config.js
    Fixup env var lost.
    
    Signed-off-by: wanorn <66821179+wanorn@users.noreply.github.com>

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,7 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
+  console.warn('This could leak super secret stuffs!');
   return {
     presets: ['babel-preset-expo'],
-    plugins: [['inline-dotenv', { systemVar: 'overwrite' }]]
+    plugins: [['inline-dotenv', {
+      unsafe: true,
+      systemVar: 'all'
+    }]]
   };
 };
