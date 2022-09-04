c845f55c68742b67eaf351b4f6078bed4d2a21ab
wanorn <66821179+wanorn@users.noreply.github.com>
2022-09-04 16:49:46 +0700

    Update index.ts
    Implement DGraph Auth request from lambda.
    
    Signed-off-by: wanorn <66821179+wanorn@users.noreply.github.com>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,15 @@
 import { addGraphQLResolvers, addMultiParentGraphQLResolvers } from '@slash-graphql/lambda-types';
 
 addGraphQLResolvers({
-    'Query.version': () => '0.0.0',
+    'Query.version': ({ authHeader }) => {
+        if(!authHeader) return 'Access Denied (No Auth header)';
+        if(!authHeader.value) return 'Access Denied (No Auth Content)';
+        const headerValue = authHeader.value;
+        if (headerValue === "") return 'Access Denied (Empty Auth Content)';
+        const base64Url = headerValue.split(".")[1];
+        const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+        const jwtPayload = JSON.parse(atob(base64));
+        return `0.0.0 (Requested from ${jwtPayload.name})`;
+    },
 });
 addMultiParentGraphQLResolvers({});
