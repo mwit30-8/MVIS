99bf2e9fb7006a9710cc5be6cba3766a8c12ef9c
HanchaiN <85230240+HanchaiN@users.noreply.github.com>
2022-09-10 23:36:20 +0700

    Add authentication context provider in storybook.

--- frontend/src/components/Authenticate/Authenticate.stories.tsx
+++ frontend/src/components/Authenticate/Authenticate.stories.tsx
@@ -2,10 +2,11 @@ import { action } from '@storybook/addon-actions';
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
 import Authenticate from '.';
+import AuthContextProvider from './context';
 import CenterView from '../CenterView';
 
 storiesOf('Authenticate', module)
-  .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
-  .add('with text', () => (
-    <Authenticate onSignIn={action('change-jwt')} />
+  .addDecorator((getStory) => <CenterView><AuthContextProvider>{getStory()}</AuthContextProvider></CenterView>)
+  .add('Default', () => (
+    <Authenticate onSignIn={action('signed-in')} onSignOut={action('signed-out')} />
   ));
